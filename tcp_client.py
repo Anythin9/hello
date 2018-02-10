5349453f2c2ac4bea6b3a2b6e95ab4f1fcce5529
Hubot <609522704@qq.com>
2018-02-10 13:58:22 +0800

    hello

---
+++ tcp_client.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import socket
+
+target_host = 'www.91tk.net'
+target_port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((target_host, target_port))
+
+client.send('GET / HTTP/1.1\r\nHost: 91tk.net\r\n\r\n')
+response = client.recv(4096)
+
+print response
+
+
